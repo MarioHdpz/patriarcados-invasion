d629a06ff345613eab6cb97d1cb7ac624e5e1844
Mario <mariohdpz@gmail.com>
2020-02-13 20:42:37 -0600

    Fix game title

--- game.js
+++ game.js
@@ -39,7 +39,7 @@ var startGame = function() {
     Game.setBoard(1,new Starfield(50,0.6,100));
     Game.setBoard(2,new Starfield(100,1.0,50));
   }  
-  Game.setBoard(3,new TitleScreen("Patiarcado's Invasion", 
+  Game.setBoard(3,new TitleScreen("Patriarcado's Invasion", 
                                   "Presiona fire para iniciar",
                                   messageTwo));
 };
